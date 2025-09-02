8136c3ffaed87cd24e83d183c8ab2877d4a9ec3c
PraveenBojja83 <Praveen.bojja1983@hotmail.com>
2025-09-02 17:03:28 +0530

    Initial commit:removed password  added backend, frontend, and package.json

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const db = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  password: "Madhuri@1990",
+  password: "XXXXXXXX",
   database: "praveen_db"
 });
 
